561b62d5bfcc69c2052a7e0b7ce2e080885a7dcb
john lester escarlan <jlescarlan11@gmail.com>
2025-04-27 17:45:59 +0800

    added a sign up form

--- controllers/storifyController.ts
+++ controllers/storifyController.ts
@@ -3,3 +3,7 @@ import type { RequestHandler } from "express";
 export const index: RequestHandler = (req, res) => {
   res.render("index");
 };
+
+export const signUpGet: RequestHandler = (req, res) => {
+  res.render("signUpForm");
+};
--- routes/storifyRouter.ts
+++ routes/storifyRouter.ts
@@ -3,5 +3,6 @@ const storifyRouter = Router();
 const storifyController = require("../controllers/storifyController");
 
 storifyRouter.get("/", storifyController.index);
+storifyRouter.get("/signup", storifyController.signUpGet);
 
 export default storifyRouter;
