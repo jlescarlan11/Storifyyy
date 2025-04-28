4fcac63ed699bf877b5c954e28bb5660c8a7c5fc
john lester escarlan <jlescarlan11@gmail.com>
2025-04-29 01:07:21 +0800

    get back to the basics.

--- routes/storifyRouter.ts
+++ routes/storifyRouter.ts
@@ -4,5 +4,6 @@ const storifyController = require("../controllers/storifyController");
 
 storifyRouter.get("/", storifyController.index);
 storifyRouter.get("/signup", storifyController.signUpGet);
+storifyRouter.post("/signup", storifyController.signUpPost);
 
 export default storifyRouter;
